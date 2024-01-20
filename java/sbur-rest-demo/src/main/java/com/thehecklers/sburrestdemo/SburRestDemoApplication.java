f63be7f4d821f0a6e0c6896ef9f62ac1d42d21d3
jeremy-wang-lin <51841335+jeremy-wang-lin@users.noreply.github.com>
2024-01-20 21:30:53 +0800

    my chapter 5 end

--- java/sbur-rest-demo/src/main/java/com/thehecklers/sburrestdemo/SburRestDemoApplication.java
+++ java/sbur-rest-demo/src/main/java/com/thehecklers/sburrestdemo/SburRestDemoApplication.java
@@ -1,7 +1,12 @@
 package com.thehecklers.sburrestdemo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,13 +18,21 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.util.List;
 import java.util.Optional;
+import java.util.Random;
 import java.util.UUID;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class SburRestDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SburRestDemoApplication.class, args);
+	}
+
+	@Bean
+	@ConfigurationProperties(prefix = "droid")
+	Droid createDroid() {
+		return new Droid();
 	}
 
 }
@@ -116,5 +129,107 @@ class Coffee {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+}
+
+@RestController
+@RequestMapping("/greeting")
+class GreetingController {
+
+	private final Greeting greeting;
+
+	public GreetingController(Greeting greeting) {
+		this.greeting = greeting;
+	}
+
+	@GetMapping
+	String getGreeting() {
+		return greeting.getName();
+	}
+
+	@GetMapping("/coffee")
+	String getNameAndCoffee() {
+		return greeting.getCoffee();
+	}
+}
+
+@ConfigurationProperties(prefix = "greeting")
+class Greeting {
+	private String name;
+	private String coffee;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCoffee() {
+		return coffee;
+	}
+
+	public void setCoffee(String coffee) {
+		this.coffee = coffee;
+	}
+}
+
+class Droid {
+	private String id;
+	private String description;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
+
+@RestController
+@RequestMapping("/droid")
+class DroidController {
+	private final Droid droid;
+
+	public DroidController(Droid droid) {
+		this.droid = droid;
+	}
+
+	@GetMapping
+	Droid getDroid() {
+		return droid;
+	}
+}
+
+
+@Component
+class PackageServerHealthIndicator implements HealthIndicator {
+
+	@Override
+	public Health health() {
+		// Your custom logic to check health
+		boolean customComponentIsHealthy = checkCustomComponentHealth();
+
+		if (customComponentIsHealthy) {
+			return Health.up().withDetail("CustomComponent", "Healthy").build();
+		} else {
+			return Health.down().withDetail("CustomComponent", "Not Healthy").build();
+		}
+	}
+
+	private boolean checkCustomComponentHealth() {
+		// Implement your custom health check logic
+
+		return new Random().nextBoolean(); // or false based on the health of your component
 	}
 }
